f2a5ff6897531147010d971c249fec6a47a285c8
黄磊 <huanglei0704@thundersoft.com>
2021-06-09 21:08:36 +0800

    fix code spec
    Signed-off-by: 黄磊 <huanglei0704@thundersoft.com>
    Change-Id: I96d0f9fea061c850a86f2eed612a5adc3de00749
    Signed-off-by: 黄磊 <huanglei0704@thundersoft.com>

--- python3_action.py
+++ python3_action.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -7,20 +8,4 @@
 import subprocess
 import sys
 
-if sys.version_info.major == 3:
-  # If we get here, we're already Python3, so just re-execute the
-  # command without the wrapper.
-  exe = sys.executable
-elif sys.executable.endswith('.exe'):
-  # If we get here, we're a Python3 executable likely running on
-  # Windows, so look for the Python3 wrapper in depot_tools. We
-  # can't invoke it directly because some command lines might exceed the
-  # 8K commamand line length limit in cmd.exe, but we can use it to
-  # find the underlying executable, which we can then safely call.
-  exe = subprocess.check_output(
-      ['python.bat', '-c',
-       'import sys; print(sys.executable)']).decode('utf8').strip()
-else:
-  exe = 'python3'
-
-sys.exit(subprocess.call([exe] + sys.argv[1:]))
+sys.exit(subprocess.call(['python3'] + sys.argv[1:]))
--- wayland_scanner_wrapper.py
+++ wayland_scanner_wrapper.py
@@ -15,44 +15,64 @@ import subprocess
 import sys
 
 def generate_code(wayland_scanner_cmd, code_type, path_in, path_out):
-  ret = subprocess.call([wayland_scanner_cmd, code_type, path_in, path_out])
-  if ret != 0:
-    raise RuntimeError("wayland-scanner returned an error: %d" % ret)
+    """
+        [generate code]
+        Args:
+            wayland_scanner_cmd : wayland_scanner_cmd
+            code_type : type
+            path_in : path_in
+            path_out : path_out
+        Returns:
+            none: none 
+        Raises:
+            IOError: wayland-scanner returned an error
+    """
+    ret = subprocess.call([wayland_scanner_cmd, code_type, path_in, path_out])
+    if ret != 0:
+        raise RuntimeError("wayland-scanner returned an error: %d" % ret)
 
-def main(argv):
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--cmd", help="wayland-scanner command to execute")
-  parser.add_argument("--src-root", help="Root source directory")
-  parser.add_argument("--root-gen-dir", help="Directory for generated files")
-  parser.add_argument("protocols", nargs="+",
-                      help="Input protocol file paths relative to src root.")
 
-  options = parser.parse_args()
-  cmd = os.path.realpath(options.cmd)
-  src_root = options.src_root
-  root_gen_dir = options.root_gen_dir
-  protocols = options.protocols
+def main():
+    """
+        [main]
+        Args: NA
+        Returns: NA
+        Raises: NA
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cmd", help="wayland-scanner command to execute")
+    parser.add_argument("--src-root", help="Root source directory")
+    parser.add_argument("--root-gen-dir", help="Directory for generated files")
+    parser.add_argument("protocols", nargs="+",
+                        help="Input protocol file paths relative to src root.")
 
-  version = subprocess.check_output([cmd, '--version'],
-                                    stderr=subprocess.STDOUT)
-  # The version is of the form "wayland-scanner 1.18.0\n"
-  version = tuple([int(x) for x in version.decode().strip().split(' ')[1].split('.')])
+    options = parser.parse_args()
+    cmd = os.path.realpath(options.cmd)
+    src_root = options.src_root
+    root_gen_dir = options.root_gen_dir
+    protocols = options.protocols
 
-  for protocol in protocols:
-    protocol_path = os.path.join(src_root, protocol)
-    protocol_without_extension = protocol.rsplit(".", 1)[0]
-    out_base_name = os.path.join(root_gen_dir, protocol_without_extension)
-    code_cmd = 'private-code' if version > (1, 14, 90) else 'code'
-    generate_code(cmd, code_cmd, protocol_path,
-                  out_base_name + "-protocol.c")
-    generate_code(cmd, "client-header", protocol_path,
-                  out_base_name + "-client-protocol.h")
-    generate_code(cmd, "server-header", protocol_path,
-                  out_base_name + "-server-protocol.h")
+    version = subprocess.check_output([cmd, '--version'], \
+            stderr=subprocess.STDOUT)
+    # The version is of the form "wayland-scanner 1.18.0\n"
+    ver = version.decode().strip().split(' ')[1].split('.')
+    version = tuple([int(x) for x in ver])
+
+    for protocol in protocols:
+        protocol_path = os.path.join(src_root, protocol)
+        protocol_without_extension = protocol.rsplit(".", 1)[0]
+        out_base_name = os.path.join(root_gen_dir, protocol_without_extension)
+        code_cmd = 'private-code' if version > (1, 14, 90) else 'code'
+        generate_code(cmd, code_cmd, protocol_path,
+                      out_base_name + "-protocol.c")
+        generate_code(cmd, "client-header", protocol_path,
+                      out_base_name + "-client-protocol.h")
+        generate_code(cmd, "server-header", protocol_path,
+                      out_base_name + "-server-protocol.h")
 
 if __name__ == "__main__":
-  try:
-    main(sys.argv)
-  except RuntimeError as e:
-    print(e, file=sys.stderr)
-    sys.exit(1)
+    try:
+        main()
+    except RuntimeError as error:
+        print(error, file=sys.stderr)
+        sys.exit(1)
